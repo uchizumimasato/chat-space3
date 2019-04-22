483d858b9ac746cc2327f2c307409c8b73e0588d
uchizumimasato <uchizumimasato@users.noreply.github.com>
2019-04-22 17:13:28 +0900

    delete respond_to

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    @last_messages = Message.where('id > ?', params[:id])
-    respond_to do |format|
-      format.json
-    end
+    @last_messages = Message.where("id > ? and group_id = ?", params[:last_message_id], params[:group_id])
   end
 end
